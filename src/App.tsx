393c457d27516b55e3f2f2bd00776b82f8d1be9d
John Kornegay <jakornegay01@gmail.com>
2023-12-05 17:30:26 -0600

    removed unused assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import '@mantine/core/styles.css';
-import '@mantine/carousel/styles.css';
 import { MantineProvider } from '@mantine/core';
 import { Router } from './Router';
 import { theme } from './theme';
