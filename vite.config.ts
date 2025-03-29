b82d28f3e10496e794f13b48f274f1ba8af44c88
John Kornegay <91643982+johnathew@users.noreply.github.com>
2025-03-29 15:28:21 -0500

    error fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import netlifyPlugin from "@netlify/vite-plugin-react-router";
+import { ghPages } from "vite-plugin-gh-pages";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [reactRouter(), tailwindcss(), netlifyPlugin()],
+  plugins: [reactRouter(), tailwindcss(), ghPages()],
 });
