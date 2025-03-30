b7a45822abad844e557c86f83a44f69dec482543
John Kornegay <91643982+johnathew@users.noreply.github.com>
2025-03-30 15:25:39 -0500

    routing error fix

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,18 +1,20 @@
+import { Link } from "react-router";
+
 export const Header = () => {
 
     return (
         <header className="w-full p-2 border-b-[0.5px] text-cyan-800 bg-slate-950 flex justify-start">
-            <a href="/" aria-describedby="home" className="text-cyan-400 px-2 mx-2 border-2 rounded-sm hover:bg-cyan-400 hover:text-slate-900 hover:shadow-md">
+            <Link to="/" aria-describedby="home" className="text-cyan-400 px-2 mx-2 border-2 rounded-sm hover:bg-cyan-400 hover:text-slate-900 hover:shadow-md">
                 JK
-            </a>
-            <a href="/projects" className="text-cyan-400 mx-2 hover:underline">
+            </Link>
+            <Link to="/projects" className="text-cyan-400 mx-2 hover:underline">
                 Projects
-            </a>
-            <a href="/gallery" className="text-cyan-400 mx-2 hover:underline">
+            </Link>
+            <Link to="/gallery" className="text-cyan-400 mx-2 hover:underline">
                 Gallery
-            </a>
-            <a href="/severed" className="text-cyan-400 mx-2 hover:underline">
+            </Link>
+            <Link to="/severed" className="text-cyan-400 mx-2 hover:underline">
                 Severed
-            </a>
+            </Link>
         </header>);
 };
